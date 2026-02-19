e5b68a1d54812c2e05c0341106ff74a2a0bc03f1
Lazy-Panda78 <yash.upadhyay_cs.aiml24@gla.ac.in>
2026-02-19 15:18:46 +0530

    add setup function to initialize serial communication and attach servo

--- src/main.cpp
+++ src/main.cpp
@@ -20,4 +20,12 @@ Servo myServo;
  * Initializes serial communication at 9600 baud, attaches the servo to pin 11,
  * and prints an initialization message over Serial.
  */
+void setup() {
+ Serial.begin(9600);
+
+ myServo.attach(11);
+
+ Serial.println("Servo initiate");
+}
+
 
