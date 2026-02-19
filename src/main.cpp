2d5d658b6c2a53c5880ece94e347455a9296bfbe
Lazy-Panda78 <yash.upadhyay_cs.aiml24@gla.ac.in>
2026-02-19 15:19:57 +0530

    add reverse loop for servo angle control and serial output

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,13 @@ void loop() {
   delay(500);
  }
 
- 
+ for (int i = 180; i >= 0; i -= 10) {
+  myServo.write(i);
+
+  Serial.print("Servo Angle: ");
+  Serial.println(i);
+
+  delay(500);
  }
 
 }
